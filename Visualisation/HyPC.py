92256e23b448f912af43bbe9a3ca90a8b165e38d
ciseli <36217764+ciseli@users.noreply.github.com>
2018-11-02 09:45:14 +1100

    Update HyPC.py

--- Visualisation/HyPC.py
+++ Visualisation/HyPC.py
@@ -131,7 +131,7 @@ def saveHyPC(hypc_obj,fname):
         pickle.dump(hypc_obj, f, pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
-    column_names = ['x','y','z','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21']
+    column_names = ['x','y','z','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21']  # Specifies the column labels of the input ASCII
     cloud = hypc(inputFname)
 
 
